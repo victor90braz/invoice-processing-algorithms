6d4181b5f2fddf962a8dc1e9e97bbb9d69b602a7
dev-victor-braz <victor.90braz@gmail.com>
2025-02-16 00:57:27 +0100

    refactor logic

--- inmaticpart2/tests/unit/service/accounting_invoice_service_test.py
+++ inmaticpart2/tests/unit/service/accounting_invoice_service_test.py
@@ -8,12 +8,13 @@ from inmaticpart2.database.factories.invoice_factory import InvoiceModelFactory
 class AccountingInvoiceServiceTest(TestCase):
 
     def setUp(self):
-        self.invoice1 = InvoiceModelFactory.build_invoice(number="F2023/01", date=datetime(2023, 1, 15).date())
-        self.invoice2 = InvoiceModelFactory.build_invoice(number="F2023/02", date=datetime(2023, 1, 20).date())
-        self.invoice3 = InvoiceModelFactory.build_invoice(number="F2023/03", date=datetime(2023, 2, 10).date())
+        
+        self.invoice1 = InvoiceModelFactory.build_invoice(number="F2023/01", date=datetime(2023, 1, 15).date(), supplier="Telefónica")
+        self.invoice2 = InvoiceModelFactory.build_invoice(number="F2023/02", date=datetime(2023, 1, 20).date(), supplier="Telefónica")
+        self.invoice3 = InvoiceModelFactory.build_invoice(number="F2023/03", date=datetime(2023, 2, 10).date(), supplier="Telefónica")
+        self.invoice4 = InvoiceModelFactory.build_invoice(number="F2023/04", date=datetime(2023, 1, 15).date(), supplier="Vodafone")
 
     def test_it_correctly_checks_for_missing_invoice_numbers(self):
-
         # Arrange
         invoices = [self.invoice1, self.invoice2, self.invoice3]
         
@@ -21,15 +22,15 @@ class AccountingInvoiceServiceTest(TestCase):
         actual_result = AccountingInvoiceService().create_accounting_entries(invoices)
 
         # Assert
-        self.assertIn("F2023/04", actual_result["missing_invoice_numbers"])
-        self.assertEqual(len(actual_result["missing_invoice_numbers"]), 37)
-        self.assertEqual(
-            actual_result["missing_invoice_numbers"][:5],
+        missing_invoices = actual_result["missing_invoice_numbers"]
+        
+        self.assertEqual(len(missing_invoices), 37)
+        self.assertListEqual(
+            missing_invoices[:5],
             ["F2023/04", "F2023/05", "F2023/06", "F2023/07", "F2023/08"]
         )
 
     def test_it_correctly_validates_invoice_number_format(self):
-
         # Arrange
         valid_invoice_numbers = [self.invoice1.number, self.invoice2.number, self.invoice3.number]
         invoice_processor = AccountingInvoiceService()
@@ -42,7 +43,6 @@ class AccountingInvoiceServiceTest(TestCase):
             invoice_processor.validate_invoice_format(["F2023/01", "2023-02-03", "F2023/03"])
 
     def test_it_correctly_sorts_invoices_by_date(self):
-
         # Arrange
         invoices = [self.invoice3, self.invoice1, self.invoice2]
         invoice_processor = AccountingInvoiceService()
@@ -52,11 +52,9 @@ class AccountingInvoiceServiceTest(TestCase):
 
         # Assert
         sorted_invoice_numbers = [invoice.number for invoice in sorted_invoices]
-        expected_result = [self.invoice1.number, self.invoice2.number, self.invoice3.number]
-        self.assertEqual(sorted_invoice_numbers, expected_result)
+        self.assertListEqual(sorted_invoice_numbers, [self.invoice1.number, self.invoice2.number, self.invoice3.number])
 
     def test_it_raises_value_error_for_invalid_invoice_amount(self):
-
         # Arrange
         self.invoice1.total_value = Decimal("-100.00")
         invoice_processor = AccountingInvoiceService()
@@ -68,7 +66,6 @@ class AccountingInvoiceServiceTest(TestCase):
         self.assertEqual(str(context.exception), f"Invoice {self.invoice1.number} with amount -100.00 is not valid.")
 
     def test_it_detects_duplicate_invoice_numbers(self):
-
         # Arrange
         invoices = [self.invoice1, self.invoice1, self.invoice3]
         invoice_processor = AccountingInvoiceService()
@@ -81,16 +78,31 @@ class AccountingInvoiceServiceTest(TestCase):
         self.assertEqual(len(duplicate_invoice_numbers), 1)
 
     def test_it_correctly_groups_invoices_by_supplier_and_month(self):
-        
         # Arrange
-        invoices = [self.invoice1, self.invoice2, self.invoice3]
+        invoices = [self.invoice1, self.invoice2, self.invoice3, self.invoice4]
         invoice_processor = AccountingInvoiceService()
 
         # Act
         grouped_invoices = invoice_processor.group_invoices_by_supplier_and_month(invoices)
 
-        # Assert
-        self.assertIn(self.invoice1.supplier, grouped_invoices)
-        self.assertIn(self.invoice1.date.strftime('%Y-%m'), grouped_invoices[self.invoice1.supplier])
-        self.assertEqual(len(grouped_invoices[self.invoice1.supplier][self.invoice1.date.strftime('%Y-%m')]["invoices"]), 2)
-        self.assertEqual(len(grouped_invoices[self.invoice3.supplier][self.invoice3.date.strftime('%Y-%m')]["invoices"]), 1)
+        # Assert 
+        self.assertIn("Telefónica", grouped_invoices)
+        self.assertIn(self.invoice1.date.strftime('%Y-%m'), grouped_invoices["Telefónica"])
+        self.assertEqual(len(grouped_invoices["Telefónica"][self.invoice1.date.strftime('%Y-%m')]["invoices"]), 2)
+
+        
+        actual_base_value_telef = self.invoice1.base_value + self.invoice2.base_value
+        actual_total_value_telef = self.invoice1.total_value + self.invoice2.total_value
+
+        
+        self.assertEqual(grouped_invoices["Telefónica"][self.invoice1.date.strftime('%Y-%m')]["total_base"], actual_base_value_telef)
+        self.assertEqual(grouped_invoices["Telefónica"][self.invoice1.date.strftime('%Y-%m')]["total_value"], actual_total_value_telef)
+
+        
+        self.assertIn("Vodafone", grouped_invoices)
+        self.assertIn(self.invoice4.date.strftime('%Y-%m'), grouped_invoices["Vodafone"])
+        self.assertEqual(len(grouped_invoices["Vodafone"][self.invoice4.date.strftime('%Y-%m')]["invoices"]), 1)
+
+        
+        self.assertEqual(grouped_invoices["Vodafone"][self.invoice4.date.strftime('%Y-%m')]["total_base"], self.invoice4.base_value)
+        self.assertEqual(grouped_invoices["Vodafone"][self.invoice4.date.strftime('%Y-%m')]["total_value"], self.invoice4.total_value)
